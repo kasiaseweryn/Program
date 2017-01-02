c1cfc135b00d67f7c4ef8f26b26f30ca0b7196f6
zbytspokojna <abkitsune@gmail.com>
2017-01-02 22:01:25 +0100

    Delete Villagers.java

--- Battle/Villagers.java
+++
@@ -1,9 +0,0 @@
-package Battle;
-
-import java.util.ArrayList;
-
-public class Villagers {
-        public ArrayList<Squad> squads;
-        public Village target;
-        public int state;  //0-loss, 1-win, 2-figth
-}
