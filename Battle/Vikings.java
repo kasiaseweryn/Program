619000a23c7fe06d092ffc1869f2f711e1cdc4e9
zbytspokojna <abkitsune@gmail.com>
2017-01-02 22:02:40 +0100

    Delete Vikings.java

--- Battle/Vikings.java
+++
@@ -1,9 +0,0 @@
-package Battle;
-
-import java.util.ArrayList;
-
-public class Vikings {
-    public ArrayList<Squad> squads;
-    public Village target;
-    public int state;  //0-loss, 1-win, 2-fight
-}
