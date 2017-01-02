0665da819721487803723d58a6919909694162cb
zbytspokojna <abkitsune@gmail.com>
2017-01-02 22:02:48 +0100

    Delete Squad.java

--- Battle/Squad.java
+++
@@ -1,9 +0,0 @@
-package Battle;
-
-import java.util.ArrayList;
-
-public class Squad {
-    public ArrayList<Warior> warriors;
-    public Building target;
-    public int state;  //0-all_dead, 1-fight, 2-retreat
-}
